ac0ee98b29e4b44e7b480ce39caa1e0eba457e79
Suhas V Arudi <avsuhas@gmail.com>
2016-12-06 18:34:30 -0500

    Create 3.c

---
+++ Mind-Teasers/3.c
@@ -0,0 +1,27 @@
+/* void (*fptr)():
+    fptr is a pointer that points to a function which receives nothing and returns nothing
+                Date : 30 June 2016
+ */
+
+#include<stdio.h>
+void fun();  // function prototype
+
+int main()
+{
+  int sun;
+  void(*fptr)();
+  fptr = fun;
+  fptr();
+}
+
+void fun()
+{
+  int a,b,sum;
+  printf("enter the numbers\n");
+  scanf("%d%d",&a,&b);
+  sum = a+b;
+  printf("sum = %d\n",sum);
+}
+
+   
+  
