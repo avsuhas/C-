96e34432073b85f6688949b0d2bc8cd47618ac0a
Suhas V Arudi <avsuhas@gmail.com>
2016-12-13 23:29:39 -0500

    Create OddEven.c

---
+++ Some-basic-programs/OddEven.c
@@ -0,0 +1,17 @@
+/* Program to find odd or even */
+
+
+#include<stdio.h>
+
+int main()
+{
+        int num;
+        printf("Enter the number\n");
+        scanf("%d",&num);
+        if((num%2 == 0))
+        {
+        printf("Even");
+        }
+        else
+        printf("Odd");
+}
