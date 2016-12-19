37db223c244525f06a98669c9650d0946e6eb673
Suhas V Arudi <avsuhas@gmail.com>
2016-12-19 18:23:45 -0500

    Add files via upload

---
+++ Strings/strncmp.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int my_strncmp(char *,char *,int);
+
+int main(int argc, char *argv[])
+
+{
+
+	int n = atoi(argv[3]);
+
+	int r = my_strncmp(argv[1] , argv[2], n);
+
+	printf("%d\n",r);
+
+
+}
+
+int my_strncmp(char *p, char * q, int n)
+
+{
+
+	int i;
+
+                 for(i = 0;i< n;p++,q++,i++)
+
+                 {
+             
+		if(*p != *q)
+
+		return (*p - *q);
+
+	}
+
+	return 0;
+
+}
+
+
