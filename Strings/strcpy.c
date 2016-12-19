a372448b8f0035d4b3d89ba416b5b5551d4448a8
Suhas V Arudi <avsuhas@gmail.com>
2016-12-18 22:51:01 -0500

    Create strcpy.c

---
+++ Strings/strcpy.c
@@ -0,0 +1,33 @@
+/* string copy */
+
+
+#include<stdio.h>
+
+void my_strcpy(char *p,char*q);
+
+int main(int argc,char* argv[])
+
+{
+
+        int i;
+
+        
+my_strcpy(argv[1],argv[2] );
+
+        printf("%s\n",argv[1]);
+
+}
+
+void my_strcpy(char *p,char*q)
+
+{
+
+        while(*p++ = *q++)
+        
+        {
+                
+        
+        }
+        
+
+}
