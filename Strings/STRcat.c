48983b71c7a8202be2da60f6f533744d854ab515
Suhas V Arudi <avsuhas@gmail.com>
2016-12-19 18:24:51 -0500

    Add files via upload

---
+++ Strings/STRcat.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+void  my_strcat(char *,char *);
+
+int main(int argc, char*argv[])
+
+{
+
+
+        my_strcat(argv[1] , argv[2]);
+
+        printf("%s",argv[1]);
+
+}
+
+void my_strcat(char *p,char *q)
+
+{
+
+
+        while(*p != '\0')
+
+        p++;
+
+        while(*p++ = *q++ )
+
+	;
+
+
+}
+
+
