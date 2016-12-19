f72e039c084bfa33cbde640387ffab05185eb152
Suhas V Arudi <avsuhas@gmail.com>
2016-12-19 18:22:43 -0500

    Add files via upload

---
+++ Strings/str_str.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+
+char * my_strstr(char *,char *);
+
+int main(int argc, char * argv[])
+
+{
+
+	int i = 0;
+
+	char *str = my_strstr(argv[1], argv[2]);
+
+	printf("%p  %s",str,str);
+
+}
+
+char *my_strstr(char *s1,char *s2)
+
+{
+
+	int i,j,k,flag = 0;
+
+	for(i = 0; *(s1+i)!= '\0';i++)
+
+	{
+
+		if(*(s1+i) == *(s2+0))
+	
+	                  {
+
+			flag = 1;
+
+			for(j = 0,k = i;*(s2+j)!= '\0';j++,k++)
+
+			{
+
+				if(*(s1+k) != *(s2+j))
+
+				{
+
+
+					flag = 0;
+
+					break;
+
+				}
+
+			}
+
+		}
+
+		if(flag)
+
+		break;
+
+	}
+
+	if(flag)
+
+		return (s1 +i);
+
+	else
+
+		return NULL;
+
+}
+
