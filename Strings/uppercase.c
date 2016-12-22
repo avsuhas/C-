a3fb976c34a1a49f2f5cfce9c3d2cb784b04a039
Suhas V Arudi <avsuhas@gmail.com>
2016-12-21 22:38:48 -0500

    Add files via upload

---
+++ Strings/uppercase.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+void my_convert(char*);
+
+int main( int argc,char *argv[])
+
+{
+
+
+	int i = 1;
+
+	my_convert(argv[1]);
+	
+                  printf("after conversion string = %s\n",argv[1]);
+
+}
+
+void my_convert(char*p)
+
+{
+
+	while(*p != '\0')
+
+	{
+
+		if(*p >= 'A' && *p <= 'Z')
+
+		{
+
+			*p = *p + 32;
+
+		}
+		
+                                   else if(*p >= 'a' && *p <= 'z')
+
+		{
+
+			*p = *p - 32;
+
+		}
+
+		p++;
+
+	}
+
+} 
+	
+				
