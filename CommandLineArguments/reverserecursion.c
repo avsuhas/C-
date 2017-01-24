0b11017943d9592c81a776bdbb67f34512f6ba89
Suhas V Arudi <avsuhas@gmail.com>
2017-01-23 22:18:30 -0500

    Add files via upload

---
+++ CommandLineArguments/reverserecursion.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+#include<string.h>
+
+char* my_strrev(char*s);
+
+int main(int argc,char *argv[])
+
+{
+
+	char*p = my_strrev(argv[1]);
+
+	printf("%s",p);
+ 
+  
+}
+
+char* my_strrev(char*s)
+
+{
+
+	return (!*s)? s:strcpy(s,strncat(my_strrev(s +1),s,1));
+
+}
