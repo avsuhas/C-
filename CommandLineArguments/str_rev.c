6af893b8e425082d8f8eba45e953fd7f09169b8f
Suhas V Arudi <avsuhas@gmail.com>
2017-01-24 22:18:29 -0500

    Add files via upload

---
+++ CommandLineArguments/str_rev.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+#include<string.h>
+
+void my_strrev(char*);
+
+int main(int argc,char* argv[])
+
+{
+
+ 
+       my_strrev(argv[1]);
+ 
+       printf("%s\n",argv[1]);
+
+}
+
+
+void my_strrev(char *p1)
+
+{
+ 
+       char *p2 = p1+ strlen(p1)-1;
+
+        while(p2 > p1)
+ 
+       {
+  
+              char  temp;
+
+              temp = *p1;
+ 
+               *p1 = *p2;
+ 
+               *p2 = temp;
+
+                p1++;
+
+                p2--;
+ 
+       }
+
+
+}
+
