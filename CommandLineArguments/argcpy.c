92e2fcddd1dbae32717fa70914d08a7258062469
Suhas V Arudi <avsuhas@gmail.com>
2017-01-29 21:26:38 -0500

    Add files via upload

---
+++ CommandLineArguments/argcpy.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void my_strcpy(char *p,char*q);
+
+int main(int argc,char* argv[])
+
+{
+
+        int i;
+
+        
+my_strcpy(argv[1],argv[2] );
+
+        printf("%s\n",argv[1]);
+
+}
+
+void my_strcpy(char *p,char*q)
+
+{
+
+        while(*p++ = *q++)
+        
+        {
+                
+        
+        }
+        
+
+}
+
